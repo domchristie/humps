2d8d6d2588c1de07022aca797911184d5dc59b56
Jack Chu <jack@jackchu.com>
2015-08-18 17:54:10 -0400

    add option to ignore numbers when separating words

--- humps.js
+++ humps.js
@@ -11,8 +11,8 @@
 
 ;(function(global) {
 
-  var _processKeys = function(convert, obj, separator) {
-    if(!_isObject(obj) || _isDate(obj) || _isRegExp(obj)) {
+  var _processKeys = function(convert, obj, separator, ignoreNumbers) {
+    if(!_isObject(obj) || _isDate(obj) || _isRegExp(obj) || _isBoolean(obj)) {
       return obj;
     }
 
@@ -23,14 +23,14 @@
     if(_isArray(obj)) {
       output = [];
       for(l=obj.length; i<l; i++) {
-        output.push(_processKeys(convert, obj[i], separator));
+        output.push(_processKeys(convert, obj[i], separator, ignoreNumbers));
       }
     }
     else {
       output = {};
       for(var key in obj) {
         if(obj.hasOwnProperty(key)) {
-          output[convert(key, separator)] = _processKeys(convert, obj[key], separator);
+          output[convert(key, separator, ignoreNumbers)] = _processKeys(convert, obj[key], separator, ignoreNumbers);
         }
       }
     }
@@ -39,11 +39,18 @@
 
   // String conversion methods
 
-  var separateWords = function(string, separator) {
+  var separateWords = function(string, separator, ignoreNumbers) {
     if (typeof separator === 'undefined') {
       separator = '_';
     }
-    return string.replace(/([a-z])([A-Z0-9])/g, '$1'+ separator +'$2');
+
+    var regexp = /([a-z])([A-Z0-9])/g;
+
+    if (ignoreNumbers) {
+      regexp = /([a-z])([A-Z])/g;
+    }
+
+    return string.replace(regexp, '$1'+ separator +'$2');
   };
 
   var camelize = function(string) {
@@ -63,8 +70,8 @@
     return camelized.substr(0, 1).toUpperCase() + camelized.substr(1);
   };
 
-  var decamelize = function(string, separator) {
-    return separateWords(string, separator).toLowerCase();
+  var decamelize = function(string, separator, ignoreNumbers) {
+    return separateWords(string, separator, ignoreNumbers).toLowerCase();
   };
 
   // Utilities
@@ -84,6 +91,9 @@
   var _isRegExp = function(obj) {
     return toString.call(obj) == '[object RegExp]';
   };
+  var _isBoolean = function(obj) {
+    return toString.call(obj) == '[object Boolean]';
+  };
 
   // Performant way to determine if obj coerces to a number
   var _isNumerical = function(obj) {
@@ -99,8 +109,8 @@
     camelizeKeys: function(object) {
       return _processKeys(camelize, object);
     },
-    decamelizeKeys: function(object, separator) {
-      return _processKeys(decamelize, object, separator);
+    decamelizeKeys: function(object, separator, ignoreNumbers) {
+      return _processKeys(decamelize, object, separator, ignoreNumbers);
     },
     pascalizeKeys: function(object) {
       return _processKeys(pascalize, object);
